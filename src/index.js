0555fa0410ad32aedeec88f8cd16d73363c3537d
maqingbo <wmaqingbo@163.com>
2021-04-12 17:51:31 +0800

    1-7

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import './index.css'
 class Header extends Component {
   render () {
     const msg = 'is good'
+    const className = 'class-red'
+
     return (
       <div>
-        <h2>这里可以放置-变量：{ msg }</h2>
-        <h2>这里可以放置-表达式：{ 1 + 2 }</h2>
-        <h2>这里可以放置-函数：{ (function () { return 'IIFE' } )() }</h2>
+        <h2>这里可以放置-变量：{msg}</h2>
+        <h2>这里可以放置-表达式：{1 + 2}</h2>
+        <h2>这里可以放置-函数：{(function () { return 'IIFE' })()}</h2>
+        <h2 className={className}>属性也可以放置变量，此处文字变红色</h2>
       </div>
     )
   }
