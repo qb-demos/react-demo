8b095cbd479b72de4b0e1855dc158b393aac893d
maqingbo <wmaqingbo@163.com>
2021-04-12 17:57:08 +0800

    1-8

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+class TitleCom extends Component {
+  render () {
+    return (
+      <h1>这里是 title 组件</h1>
+    )
+  }
+}
+
+class FooterCom extends Component {
+  render () {
+    return (
+      <h1>这里是 FooterCom 组件</h1>
+    )
+  }
+}
+
 class Header extends Component {
   render () {
     const msg = 'is good'
@@ -9,10 +25,12 @@ class Header extends Component {
 
     return (
       <div>
+        <TitleCom></TitleCom>
         <h2>这里可以放置-变量：{msg}</h2>
         <h2>这里可以放置-表达式：{1 + 2}</h2>
         <h2>这里可以放置-函数：{(function () { return 'IIFE' })()}</h2>
         <h2 className={className}>属性也可以放置变量，此处文字变红色</h2>
+        <FooterCom></FooterCom>
       </div>
     )
   }
