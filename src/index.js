4df0228b84c9134729caf5495526846377a77fd5
a-maqingbo <qingbo.ma@symbio.com>
2021-04-12 17:03:02 +0800

    react.js 小书：1-7；

--- src/index.js
+++ src/index.js
@@ -1,65 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-class Square extends React.Component {
-  render () {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
-}
-class Board extends React.Component {
-  renderSquare (i) {
-    return <Square />;
-  }
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
+class Header extends Component {
   render () {
-    const status = 'Next player: X';
-
+    const msg = 'is good'
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        <h2>这里可以放置-变量：{ msg }</h2>
+        <h2>这里可以放置-表达式：{ 1 + 2 }</h2>
+        <h2>这里可以放置-函数：{ (function () { return 'IIFE' } )() }</h2>
       </div>
-    );
+    )
   }
 }
-
-class Game extends React.Component {
-  render () {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Header />,
   document.getElementById('root')
-);
+)
