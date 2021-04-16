9889339de9e8366f56ff2d4c3d9042b42ba9ad2e
maqingbo <wmaqingbo@163.com>
2021-04-16 15:48:22 +0800

    chore: 2-16：用户名保存到本地，评论框聚焦；

--- src/comment/CommentInput.js
+++ src/comment/CommentInput.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 
 class CommentInput extends Component {
 
+  // static defaultProps = {}
+  // static propTypes = {}
+
   constructor() {
     super()
     this.state = {
@@ -10,10 +13,23 @@ class CommentInput extends Component {
     }
   }
 
+  UNSAFE_componentWillMount () {
+    const userName = localStorage.getItem('userName')
+    if (userName) {
+      this.setState({ userName })
+    }
+  }
+  componentDidMount () {
+    this.textarea.focus()
+  }
+
   handleUserNameChanged (e) {
     this.setState({
       userName: e.target.value
     })
+  }
+  handleUsernameBlur (e) {
+    localStorage.setItem('userName', e.target.value)
   }
   handleContentChanged (e) {
     this.setState({
@@ -35,13 +51,19 @@ class CommentInput extends Component {
         <div className="comment-field">
           <span className="comment-filed-name">用户名：</span>
           <div className="comment-field-input">
-            <input value={this.state.userName} onChange={this.handleUserNameChanged.bind(this)} />
+            <input
+              value={this.state.userName}
+              onBlur={this.handleUsernameBlur.bind(this)}
+              onChange={this.handleUserNameChanged.bind(this)} />
           </div>
         </div>
         <div className="comment-field">
           <span className="comment-filed-name">评论内容：</span>
           <div className="comment-field-input">
-            <textarea value={this.state.content} onChange={this.handleContentChanged.bind(this)} />
+            <textarea
+              ref={(textarea) => this.textarea = textarea}
+              value={this.state.content}
+              onChange={this.handleContentChanged.bind(this)} />
           </div>
         </div>
         <div className='comment-field-button'>
